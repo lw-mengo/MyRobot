d89b04eec4c187fcaf46152e68b66e2538e2002e
lw-mengo <867740815@qq.com>
2016-12-08 15:18:04 +0800

    解决了SDK 连接问题，可以正确获取返回值了。

--- app/src/main/java/com/example/lemgo/myrobot/MainActivity.java
+++ app/src/main/java/com/example/lemgo/myrobot/MainActivity.java
@@ -14,8 +14,15 @@ import com.turing.androidsdk.SDKInit;
 import com.turing.androidsdk.SDKInitBuilder;
 import com.turing.androidsdk.TuringApiManager;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import turing.os.http.core.ErrorMessage;
+import turing.os.http.core.HttpConnectionListener;
+import turing.os.http.core.RequestResult;
 
 /**
  * Created by Administrator on 2016/12/7 0007.
@@ -27,8 +34,10 @@ public class MainActivity extends Activity {
     private Button send;
     private MsgAdapter adapter;
     private List<Msg> msgList  = new ArrayList<Msg>();
-
+    private TuringApiManager m = null;
     private static final String KEY = "7b2fc8d27a294814b86b93752c99c9e3";
+    private static final String SECRET = "f56ca31f6cb0c8e4";
+    private static final String UNIQUEID = "159357";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,22 +46,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.avtivity_main);
         initMsgs();
 
-        SDKInitBuilder builder = new SDKInitBuilder(MyApplication.getContext()).setTuringKey(KEY).setUniqueId("18796015450");
-        SDKInit init = new SDKInit();
-        init.init(builder, new InitListener() {
-            @Override
-            public void onComplete() {
-               TuringApiManager m = new TuringApiManager(MainActivity.this);
-
-
-            }
-
-            @Override
-            public void onFail(String s) {
-                Log.d("msg22","chucuole");
-
-            }
-        });
+        //初始化机器人
 
         adapter =new MsgAdapter(MainActivity.this,R.layout.msg_item,msgList);
         editText  = (EditText) findViewById(R.id.input_text);
@@ -62,21 +56,63 @@ public class MainActivity extends Activity {
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String content = editText.getText().toString();
+                final    String  content = editText.getText().toString();
+                SDKInitBuilder builder = new SDKInitBuilder(MainActivity.this).setTuringKey(KEY).setSecret(SECRET).setUniqueId(UNIQUEID);//这三个参数必须要
+                SDKInit.init(builder, new InitListener() {
+                    @Override
+                    public void onComplete() {
+                        m = new TuringApiManager(MainActivity.this);
+                        m.setHttpListener(httpConnectionListener);
+
+                        m.requestTuringAPI(content);//传递消息过去
+                    }
+                    @Override
+                    public void onFail(String s) {
+                        Log.d("msg22",s);
+                    }
+                });
                 if(!"".equals(content)){
                     Msg msg =new Msg(content,Msg.TYPE_SEND);
                     msgList.add(msg);
                     adapter.notifyDataSetChanged();//   当有新消息时，刷新listview的显示
                     msgListView.setSelection(msgList.size());//将listview定位到最后一行
                     editText.setText("");//清空输入框
+
                 }
             }
         });
 
     }
+    HttpConnectionListener httpConnectionListener = new HttpConnectionListener() {
+        @Override
+        public void onError(ErrorMessage errorMessage) {
+            Log.d("msg22",errorMessage.getMessage());
+        }
+
+        @Override
+        public void onSuccess(RequestResult requestResult) {
+            if(requestResult!=null){
+                try {
+                    JSONObject jsonObject = new JSONObject(requestResult.getContent().toString());
+                    if(jsonObject.has("text")){
+                        Log.d("msg223",jsonObject.getString("text"));
+                    }
+                    String content = jsonObject.getString("text");
+                    Msg msg =new Msg(content,Msg.TYPE_RECEIVED);
+                    msgList.add(msg);
+                    adapter.notifyDataSetChanged();
+                    msgListView.setSelection(msgList.size());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
 
     private void initMsgs() {
         Msg msg1 = new Msg("主人你好，有什么需要帮助的吗？",Msg.TYPE_RECEIVED);
         msgList.add(msg1);
     }
+
+
 }
